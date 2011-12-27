2f77180db951611db931c3a7970987aed95e3af4
Viktoria Krolikowski <viktoria.krolikowski@yahoo.de>
2011-12-27 17:43:49 +0100

    added new apply method to RotatingCounter with (period, size) tupel list

--- src/main/scala/de/krolikowski/counter/RotatingCounter.scala
+++ src/main/scala/de/krolikowski/counter/RotatingCounter.scala
@@ -24,4 +24,9 @@ object RotatingCounter {
   def apply(size: Int, period: Long*): RotatingCounter =
     (for(p <- period) yield new SimpleRotatingCounter(p, size).asInstanceOf[RotatingCounter]) reduceLeft
       { new StackedRotatingCounter(_, _) }
+
+  def apply(periodDetails: (Long, Int)*): RotatingCounter =
+    periodDetails map
+      { (p: (Long, Int)) => new SimpleRotatingCounter(p._1, p._2).asInstanceOf[RotatingCounter] } reduceLeft
+        { new StackedRotatingCounter(_, _) }
 }
